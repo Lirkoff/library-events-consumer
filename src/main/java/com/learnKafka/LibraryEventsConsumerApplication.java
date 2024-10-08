6659276fc5c0c0804af2d08c31b3bdbb4a67a00c
Lirkoff <martinlirkoff@gmail.com>
2024-10-08 09:35:57 +0300

    Add manual offset handling and YAML config
    Introduced a new `LibraryEventsConsumerManualOffset` class to handle Kafka messages with manual offset acknowledgment. Converted `application.properties` to `application.yaml` for better configuration management. Updated the build script to include Lombok and restructured the source sets for unit and integration tests.

--- src/main/java/com/learnKafka/LibraryEventsConsumerApplication.java
+++ src/main/java/com/learnKafka/LibraryEventsConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.learnKafka.libraryEventsConsumer;
+package com.learnKafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
